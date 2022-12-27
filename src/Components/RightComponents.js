7e3bd812a2ccd4a2d8d61d74ee0b086b91fde2ee
Shubham Singh <shubhambaba143143@gmail.com>
2022-12-28 01:31:59 +0530

    right change

--- src/Components/RightComponents.js
+++ src/Components/RightComponents.js
@@ -5,13 +5,11 @@ import { solid, regular } from '@fortawesome/fontawesome-svg-core/import.macro'
 function RightComponents() {
     return (
         <div className="right">
+            <FontAwesomeIcon icon={solid('search')} color="rgba(0,0,0,.5)" size="2x" style={{ backgroundColor: 'rgb(130,159,154)', position: 'absolute', right: 0, padding: 16, }} />
             <div className="content">
                 <div className="upperComponent">
-                    <p>Another Location</p>
-                    <FontAwesomeIcon icon={solid('search')} size="4x" style={{ backgroundColor: 'rgb(77,117,109)' }} />
-
+                    <input className="inputfield" type="text" placeholder="Another Location" />
                 </div>
-                <hr style={{ height: "2px", borderWidth: 0, color: "gray", backgroundColor: "gray" }} />
                 <div className="middleComponent">
                     <p>Haridwar</p>
                     <p>Varanasi</p>
@@ -21,26 +19,27 @@ function RightComponents() {
                 </div>
                 <hr style={{ height: "2px", borderWidth: 0, color: "gray", backgroundColor: "gray" }} />
                 <div className="weatherdetail">
-                    <b> weather detail</b>
-                    <div className="cloud">
-                        <p>Cloud</p>
-                        <p>86%</p>
+                    <b className="cloud1"> Weather Details</b>
+                    <div className="lowerContener">
+                        <div className="cloud">
+                            <p>Cloud</p>
+                            <p className="cloud1">86%</p>
+                        </div>
+                        <div className="cloud">
+                            <p>Humidity</p>
+                            <p className="cloud1">86%</p>
+                        </div>
+                        <div className="cloud">
+                            <p>Wind</p>
+                            <p className="cloud1">86%</p>
+                        </div>
+                        <div className="cloud">
+                            <p>Rain</p>
+                            <p className="cloud1">86%</p>
+                        </div>
                     </div>
-                    <div className="cloud">
-                        <p>Humidity</p>
-                        <p>86%</p>
-                    </div>
-                    <div className="cloud">
-                        <p>Wind</p>
-                        <p>86%</p>
-                    </div>
-                    <div className="cloud">
-                        <p>Rain</p>
-                        <p>86%</p>
-                    </div>
-
-
                 </div>
+                <hr style={{ height: "2px", borderWidth: 0, color: "gray", backgroundColor: "gray" }} />
             </div>
         </div>
     )
