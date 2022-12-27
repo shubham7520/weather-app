e6f9decbd1e94f42f4b2b7027dca1726ddc006a7
Shubham Singh <shubhambaba143143@gmail.com>
2022-12-27 23:03:10 +0530

    right component

--- src/Components/RightComponents.js
+++ src/Components/RightComponents.js
@@ -1,12 +1,48 @@
 import "../Style/RightComponent.css"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular } from '@fortawesome/fontawesome-svg-core/import.macro'
 function RightComponents() {
     return (
         <div className="right">
+            <div className="content">
+                <div className="upperComponent">
+                    <p>Another Location</p>
+                    <FontAwesomeIcon icon={solid('search')} size="2x" style={{ backgroundColor: 'rgb(77,117,109)' }} />
 
-            <h1> hlo</h1 >
+                </div>
+                <hr style={{ height: "2px", borderWidth: 0, color: "gray", backgroundColor: "gray" }} />
+                <div>
+                    <p>Haridwar</p>
+                    <p>Varanasi</p>
+                    <p>Prayagraj</p>
+                    <p>Patna</p>
 
-        </div >
+                </div>
+                <hr style={{ height: "2px", borderWidth: 0, color: "gray", backgroundColor: "gray" }} />
+                <div className="weatherdetail">
+                    <b> weather detail</b>
+                    <div className="cloud">
+                        <p>Cloud</p>
+                        <p>86%</p>
+                    </div>
+                    <div className="cloud">
+                        <p>Humidity</p>
+                        <p>86%</p>
+                    </div>
+                    <div className="cloud">
+                        <p>Wind</p>
+                        <p>86%</p>
+                    </div>
+                    <div className="cloud">
+                        <p>Rain</p>
+                        <p>86%</p>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
     )
 }
 
