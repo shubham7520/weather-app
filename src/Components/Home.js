2a48e874bc31f6d4980c5b5cefa9ad79b37abee3
Shubham Singh <shubhambaba143143@gmail.com>
2022-12-29 20:06:16 +0530

    some changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,12 +7,12 @@ function Home() {
     const [data, setData] = useState(null);
     const [search, setSearch] = useState("delhi");
 
-    console.log(search);
+
 
     const getData = async () => {
         await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=ddf7b73c9dabb31f70c91c29fa267797`).then(res => {
             setData(res.data);
-            console.log("ravi", res.data);
+
         }).catch((err) => {
             if (err.response.status === 404) {
                 alert('please enter a valid city name');
